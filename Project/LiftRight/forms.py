27464c1d4e0ec38630d12499be2ddb8e40f574e5
Yomna Khairy <y.awad2296@nu.edu.eg>
2024-12-17 21:02:56 +0200

    Added (gender, goal, body fat) attributes to user profile

--- Project/LiftRight/forms.py
+++ Project/LiftRight/forms.py
@@ -3,9 +3,11 @@ from django import forms
 from .models import CustomUser
 
 class CustomUserCreationForm(UserCreationForm):
+    # gender = forms.ChoiceField(choices=CustomUser.gender, required=False, widget=forms.Select())
+
     class Meta:
         model = CustomUser
-        fields = ['username', 'email', 'password1', 'password2', 'age', 'weight', 'height']
+        fields = ['username', 'email', 'password1', 'password2', 'age', 'weight', 'height','gender','goal','body_fat_percentage']
 
 class CustomAuthenticationForm(AuthenticationForm):
     class Meta:
